83a7fd065a300fde9c22cbc8751c5e1500023ed3
WhO2022 <115972850+WhO2022@users.noreply.github.com>
2023-02-04 21:56:02 +0400

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Final//RU">
+
+<HTML><HEAD><TITLE>Samir's Webnet</TITLE>
+<LINK 
+rel="shortcut icon" href="img/20.jpg" type="img/20.jpg"
+title="global css" href="cssnet.css" type=text/css 
+rel=stylesheet>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+</HEAD>
+
+<body>
+<iframe name='chatWindow' id='chatWindow' src='iframe.php'>Чатик</iframe>
+<form action='iframe.php' method='post' target='chatWindow'>
+<input type='text' name='message'>
+<input type='submit' />
+</form> 
+</body>
